145a372942a0b9af6ad9eead77cb2a2e07d04ee7
SusanBird <susanbird391@gmail.com>
2022-07-27 16:27:59 -0700

    add list items, update list css

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,3 +1,72 @@
 export default function List() {
-  return <p>I am the List page</p>;
-}
+    return (
+        <ul className={styles.List}>
+          <li>list item 1</li>
+          <li>list item 2</li>
+          <li>list item 3</li>
+          <li>list item 4</li>
+          <li>list item 5</li>
+          <li>list item 6</li>
+          <li>list item 1</li>
+          <li>list item 2</li>
+          <li>list item 3</li>
+          <li>list item 4</li>
+          <li>list item 5</li>
+          <li>list item 6</li>
+          <li>list item 1</li>
+          <li>list item 2</li>
+          <li>list item 3</li>
+          <li>list item 4</li>
+          <li>list item 5</li>
+          <li>list item 6</li>
+          <li>list item 1</li>
+          <li>list item 2</li>
+          <li>list item 3</li>
+          <li>list item 4</li>
+          <li>list item 5</li>
+          <li>list item 6</li>
+          <li>list item 1</li>
+          <li>list item 2</li>
+          <li>list item 3</li>
+          <li>list item 4</li>
+          <li>list item 5</li>
+          <li>list item 6</li>
+          <li>list item 1</li>
+          <li>list item 2</li>
+          <li>list item 3</li>
+          <li>list item 4</li>
+          <li>list item 5</li>
+          <li>list item 6</li>
+          <li>list item 1</li>
+          <li>list item 2</li>
+          <li>list item 3</li>
+          <li>list item 4</li>
+          <li>list item 5</li>
+          <li>list item 6</li>
+          <li>list item 1</li>
+          <li>list item 2</li>
+          <li>list item 3</li>
+          <li>list item 4</li>
+          <li>list item 5</li>
+          <li>list item 6</li>
+          <li>list item 1</li>
+          <li>list item 2</li>
+          <li>list item 3</li>
+          <li>list item 4</li>
+          <li>list item 5</li>
+          <li>list item 6</li>
+          <li>list item 1</li>
+          <li>list item 2</li>
+          <li>list item 3</li>
+          <li>list item 4</li>
+          <li>list item 5</li>
+          <li>list item 6</li>
+          <li>list item 1</li>
+          <li>list item 2</li>
+          <li>list item 3</li>
+          <li>list item 4</li>
+          <li>list item 5</li>
+          <li>list item 6</li>
+        </ul>
+      );
+    }
