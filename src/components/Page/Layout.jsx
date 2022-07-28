b492d7885db3157209b15fda3d2170b0727cdeeb
SusanBird <susanbird391@gmail.com>
2022-07-27 18:00:23 -0700

    small css tweaks, moved Header folder to under Page, wrote NavLink function

--- src/components/Page/Layout.jsx
+++ src/components/Page/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import Header from '../Header/Header';
+import Header from './Header/Header';
 import './reset.css';
 import './global.css';
 import styles from './Layout.css';
