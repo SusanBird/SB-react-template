1999ce84dfb8048f994562caf0e43aefd2a44c9e
SusanBird <susanbird391@gmail.com>
2022-07-27 18:16:51 -0700

    added about page form and css

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,45 @@
+import {
+  InputControl,
+  SelectControl,
+  TextAreaControl,
+  CheckboxControl,
+} from '../Forms/FormControls.jsx';
 
+import styles from './About.css';
 
 export default function About() {
-  return <p>I am the About page</p>;
+  return (
+    <section className={styles.About}>
+      Some text here...
+      <form>
+        <InputControl
+          label="email"
+          name="email"
+          placeholder="enter your email"
+          type="email"
+          required
+        />
+
+        <InputControl
+          label="password"
+          name="password"
+          placeholder="choose a password"
+          type="password"
+          required
+        />
+
+        <SelectControl label="type">
+          <option>A</option>
+          <option>B</option>
+          <option>C</option>
+        </SelectControl>
+
+        <CheckboxControl label="Easy Mode?" text="Yes" />
+
+        <TextAreaControl label="bio" placeholder="tell us about yourself" />
+
+        <button>Submit</button>
+      </form>
+    </section>
+  );
 }
