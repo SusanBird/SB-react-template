b51f80064982ad9d5bdbb3d1fbe2c50cb922e1cb
SusanBird <susanbird391@gmail.com>
2022-08-01 15:44:05 -0700

    connect to supabase and write getFamilies context

---
+++ src/State/services/client.js
@@ -0,0 +1,9 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const SUPABASE_URL =
+  'https://cwyvloetshvefkkrywzk.supabase.co';
+const SUPABASE_KEY =
+  // eslint-disable-next-line max-len
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3eXZsb2V0c2h2ZWZra3J5d3prIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc4Nzk0MzEsImV4cCI6MTk2MzQ1NTQzMX0.8lVyqbhvuR5E2KD1n187Bk0teEnC6OCfd7h2Kkj_Oew';
+
+export const client = createClient(SUPABASE_URL, SUPABASE_KEY);
