52ff9de6e6e412032d1e4ecdf8ab4e5155223bbb
SusanBird <susanbird391@gmail.com>
2022-08-01 17:58:49 -0700

    added update feature to family list page

--- src/components/FuzzyBunny/FamilyList.jsx
+++ src/components/FuzzyBunny/FamilyList.jsx
@@ -1,8 +1,10 @@
+import { useEffect, useRef, useState } from 'react';
 import {
   useFamilies,
   useActions
 } from '../../state/hooks/fuzzyBunny.js';
 import styles from './FamilyList.css';
+import { InputControl } from '../Forms/FormControls.jsx';
 
 export default function FamilyList() {
   const { families } = useFamilies();
@@ -19,12 +21,49 @@ export default function FamilyList() {
 }
 
 function Family({ family }) {
-  const { remove } = useActions();
+  const { remove, update } = useActions();
+  const [editing, setEditing] = useState(false);
+  const ref = useRef();
+  const [name, setName] = useState(family.name);
 
   const handleRemove = () => remove(family.id);
 
+  const handleDoubleClick = () => {
+    setEditing(true);
+  };
+
+  useEffect(() => {
+    if (editing) ref.current.focus();
+  }, [editing]);
+
+  const handleChange = ({ target }) => {
+    setName(target.value);
+  };
+
+  const handleEdit = async () => {
+    setEditing(false);
+    if (name === family.name) return;
+    await update({ name, id: family.id });
+  };
+
+  const handleKeyUp = (e) => {
+    if (e.code === 'Enter') handleEdit();
+  };
+
   return (
     <li className={styles.Family}>
+      {editing ? (
+        <InputControl
+          ref={ref}
+          name="name"
+          value={name}
+          onChange={handleChange}
+          onKeyUp={handleKeyUp}
+          onBlur={handleEdit}
+        />
+      ) : (
+        <h2 onDoubleClick={handleDoubleClick}>{family.name}</h2>
+      )}
       <button onClick={handleRemove}>ⓧ</button>
     </li>
   );
