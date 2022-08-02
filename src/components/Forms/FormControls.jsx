662f13ce74d7f6eb37c9d40dbbada984e35f1c68
SusanBird <susanbird391@gmail.com>
2022-08-02 11:49:05 -0700

    ensure value property exisits before defaulting to empty string

--- src/components/Forms/FormControls.jsx
+++ src/components/Forms/FormControls.jsx
@@ -78,35 +78,52 @@ export function OptionGroupControl({
   );
 }
 
-export const InputControl = forwardRef(
-  ({ label, className, value, ...rest }, ref) => {
-    return (
-      <FormControl label={label} className={className}>
-        <input ref={ref} value={value || ''} {...rest} />
-      </FormControl>
-    );
-  }
-);
+const verifyValue = (props) => {
+  if (Object.prototype.hasOwnProperty.call(props, 'value'))
+    props.value = props.value ?? '';
+};
+
+export const InputControl = forwardRef((props, ref) => {
+  const { label, className, ...rest } = props;
+  verifyValue(rest);
+
+  return (
+    <FormControl label={label} className={className}>
+      <input ref={ref} {...rest} />
+    </FormControl>
+  );
+});
 
 InputControl.displayName = 'InputControl';
 
-export function SelectControl({ label, children, value, ...rest }) {
+export const SelectControl = forwardRef((props, ref) => {
+  const { label, children, ...rest } = props;
+  verifyValue(rest); 
+  
   return (
     <FormControl label={label}>
-      <select value={value || ''} {...rest}>
+      <select ref={ref} {...rest}>
         {children}
       </select>
     </FormControl>
   );
-}
+});
 
-export function TextAreaControl({ label, ...rest }) {
+
+SelectControl.displayName = 'SelectControl';
+
+export const TextAreaControl = forwardRef((props, ref) => {
+  const { label, ...rest } = props;
+  verifyValue(rest);
+
   return (
     <FormControl label={label}>
-      <textarea {...rest}></textarea>
+      <textarea ref={ref} {...rest}></textarea>
     </FormControl>
   );
-}
+});
+
+TextAreaControl.displayName = 'TextAreaControl';
 
 export function FormButton({
   children,
