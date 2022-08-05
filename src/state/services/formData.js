21fd088e6e50737e550463d36979f862f18c14a8
SusanBird <susanbird391@gmail.com>
2022-08-05 09:37:54 -0700

    created formData.js

---
+++ src/state/services/formData.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+export function useForm() {
+  const [data, setData] = useState({});
+
+  const handleChange = ({ target }) => {
+    setData({
+      ...data,
+      [target.name]: getValue(target),
+    });
+  };
+
+  return [data, handleChange];
+}
+
+function getValue({ value, type, checked }) {
+  if (type === 'checkbox') return checked;
+  if (!isNaN(value)) return +value;
+  return value;
+}
