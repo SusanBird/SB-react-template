f2a32c2c2ccb51c1fbdba69a12e1baa545de9bfa
SusanBird <susanbird391@gmail.com>
2022-08-05 10:09:51 -0700

    move routes out of app js and into Routes jsx, css tweaks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,18 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import UserAuth from './UserAuth/UserAuth.jsx';
-import Layout from './Page/Layout.jsx';
-import Home from './Home/Home.jsx';
-import Pokedex from './Pokedex/Pokedex.jsx';
-import About from './About/About.jsx';
-import Wat from './Wat/Wat.jsx';
-import FuzzyBunny from './FuzzyBunny/FuzzyBunny.jsx';
-import Families from './FuzzyBunny/Families.jsx';
-import Bunnies from './FuzzyBunny/Bunnies.jsx';
+import Routes from './Routes.jsx';
+import UserProvider from '../state/context/UserContext.jsx';
 import FuzzyBunnyProvider from '../state/context/FuzzyBunnyContext.jsx';
 
 export default function App() {
   return (
     <Router>
       <Toaster />
-      <FuzzyBunnyProvider> 
-        <Routes>
-          <Route path="/auth" element={<UserAuth />} />
-          <Route element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="pokedex" element={<Pokedex />} />
-            <Route path="about" element={<About />} />
-            <Route path="wat" element={<Wat />} />
-            <Route path="fuzzy-bunny" element={<FuzzyBunny />}>
-              <Route index element={<Families />} />
-              <Route path="bunnies" element={<Bunnies />} />
-            </Route>
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Route>
-        </Routes>
-      </FuzzyBunnyProvider>
+      <UserProvider>
+        <FuzzyBunnyProvider>
+          <Routes />
+        </FuzzyBunnyProvider>
+      </UserProvider>
     </Router>
   );
 }
