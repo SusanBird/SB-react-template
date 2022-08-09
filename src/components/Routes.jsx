5ffe6b61fc82f229e56e14bedabd6c9136ad5d35
SusanBird <susanbird391@gmail.com>
2022-08-09 16:01:48 -0700

    protected routes

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,4 +1,7 @@
-import { Routes as RRoutes, Route } from 'react-router-dom';
+
+
+import { Routes as RRoutes, Route, Navigate } from 'react-router-dom';
+import ProtectedRoutes from './UserAuth/ProtectRoutes.jsx';
 import UserAuth from './UserAuth/UserAuth.jsx';
 import Layout from './Page/Layout.jsx';
 import Home from './Home/Home.jsx';
@@ -13,14 +16,18 @@ export default function Routes() {
     <RRoutes>
       <Route path="user/*" element={<UserAuth />} />
       <Route element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="pokedex" element={<Pokedex />} />
-        <Route path="about" element={<About />} />
-        <Route path="fuzzy-bunny" element={<FuzzyBunny />}>
-          <Route index element={<Families />} />
-          <Route path="bunnies" element={<Bunnies />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route index element={<Home />} />
+          <Route path="pokedex" element={<Pokedex />} />
+          <Route path="fuzzy-bunny" element={<FuzzyBunny />}>
+            <Route index element={<Families />} />
+            <Route path="bunnies" element={<Bunnies />} />
+          </Route>
         </Route>
-        {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
+
+        <Route path="about" element={<About />} />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </RRoutes>
   );
